231c6336105e1ecbad209fe38334c9bd00403b36
Oscar Gardiazabal <trollderiu@gmail.com>
2018-05-14 13:12:00 -0400

    Update simple_main.js

--- src/simple_main.js
+++ src/simple_main.js
@@ -5,19 +5,22 @@ define(function () {
     requirejs.config({
         waitSeconds: 0, //prevent timeout by requirejs
         paths: {
+            // REQUIREJS PLUGINS
             text: "https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min",
             css: "https://cdnjs.cloudflare.com/ajax/libs/require-css/0.1.10/css",
-            vue: "https://cdn.rawgit.com/edgardleal/require-vuejs/aeaff6db/dist/require-vuejs"
-                ,
+            vue: "https://cdn.rawgit.com/edgardleal/require-vuejs/aeaff6db/dist/require-vuejs",
+            // JS
             jquery: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery",
-            Vue2: "https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.13/vue",
+            Vue: "https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.13/vue",
             //bootstrap: "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.bundle",
             bootstrapMD: "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/4.0.2/bootstrap-material-design.umd",
             axios: "https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min",
             popper: "https://unpkg.com/popper.js@1.12.6/dist/umd/popper",
             bootstrap_notify: "https://cdnjs.cloudflare.com/ajax/libs/mouse0270-bootstrap-notify/3.1.7/bootstrap-notify.min",
-            sweetalert: "https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min"
-                ,
+            sweetalert: "https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min",
+            // CSS
+            //bootstrap_css: "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap-grid.min",
+            bootstrapMD_css: "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/4.0.2/bootstrap-material-design.min",
             'font-awesome': "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome",
             animate: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min",
         },
@@ -48,8 +51,8 @@ define(function () {
 
     // DEFFINITIONS
     window.module = ""; //prevent import bugs
-    define("vue-global", ["Vue2"], function (Vue2) {
-        window.Vue = Vue2;
+    define("vue-global", ["Vue"], function (Vue) {
+        window.Vue = Vue;
     });
     define("axios-global", ["axios"], function (axios) {
         window.axios = axios;
@@ -60,13 +63,17 @@ define(function () {
 
     define("boot", [
         // CSS
+        "css!bootstrapMD_css",
         "css!font-awesome",
         "css!animate", //"bootstrap_notify" NEED
         // JS
-        "jquery", "vue-global", "axios-global", "notify"
+        "jquery",
+        "vue-global",
+        "axios-global",
+        "notify"
     ]);
 
-    // NOT ESSENTIAL AND DELAYABLE LIBS
+    // NOT ESSENTIAL AND/OR DELAYABLE LIBS
     requirejs([
         "text",
         "css",
