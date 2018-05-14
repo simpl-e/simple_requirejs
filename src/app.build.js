ad8e7ef61bbc416ca2cf1c4e005b0051bab3d56e
Oscar Gardiazabal <trollderiu@gmail.com>
2018-05-14 13:35:10 -0400

    Create app.build.js

---
+++ src/app.build.js
@@ -0,0 +1,26 @@
+({
+    baseUrl: "src",
+    name: "main",
+    out: "main-built.js",
+    preserveLicenseComments: false,
+    findNestedDependencies: true,
+    paths: {
+        text: "../node_modules/requirejs-text/text",
+        vue: "../node_modules/require-vuejs/dist/require-vuejs",
+        css: "../node_modules/require-css/css",
+        'css-builder': "../node_modules/require-css/css-builder",
+        normalize: "../node_modules/require-css/normalize",
+        jquery: "../node_modules/jquery/dist/jquery",
+        Vue: "../node_modules/vue/dist/vue",
+        axios: "../node_modules/axios/dist/axios",
+        bootstrap: "../node_modules/bootstrap/dist/js/bootstrap.bundle",
+        bootstrapMD: "../node_modules/bootstrap-material-design/dist/js/bootstrap-material-design",
+        popper: "../node_modules/popper.js/dist/popper",
+        bootstrap_notify: "../node_modules/bootstrap-notify/bootstrap-notify",
+        sweetalert: "../node_modules/sweetalert/dist/sweetalert.min"
+        ,
+        bootstrapMD_css: "../node_modules/bootstrap-material-design/dist/css/bootstrap-material-design",
+        'font-awesome': "../node_modules/font-awesome/css/font-awesome",
+        animate: "../node_modules/animate.css/animate"
+    }
+})
