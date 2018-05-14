cc06a62499e3c0c84703f736aa005cc3327e9032
Oscar Gardiazabal <trollderiu@gmail.com>
2018-05-14 14:02:33 -0400

    Update cdn.app.build.js

--- src/cdn.app.build.js
+++ src/cdn.app.build.js
@@ -5,6 +5,8 @@
     preserveLicenseComments: false,
     findNestedDependencies: true,
     paths: {
+        simple_main: "../node_modules/simple_requirejs/src/simple_main",
+        // JS
         text: "empty:",
         vue: "empty:",
         css: "empty:",
@@ -18,7 +20,7 @@
         popper: "empty:",
         bootstrap_notify: "empty:",
         sweetalert: "empty:",
-        ,
+        // CSS
         bootstrapMD_css: "empty:",
         'font-awesome': "empty:",
         animate: "empty:"
