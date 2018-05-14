ca710ca9863a86bb384fe9fbda356938cbb8d3c0
Oscar Gardiazabal <trollderiu@gmail.com>
2018-05-14 13:41:03 -0400

    Create cdn.app.build.js

---
+++ src/cdn.app.build.js
@@ -0,0 +1,26 @@
+({
+    baseUrl: "src",
+    name: "main",
+    out: "main-built.js",
+    preserveLicenseComments: false,
+    findNestedDependencies: true,
+    paths: {
+        text: "empty:",
+        vue: "empty:",
+        css: "empty:",
+        'css-builder': "empty:",
+        normalize: "empty:",
+        jquery: "empty:",
+        Vue: "empty:",
+        axios: "empty:",
+        bootstrap: "empty:",
+        bootstrapMD: "empty:",
+        popper: "empty:",
+        bootstrap_notify: "empty:",
+        sweetalert: "empty:",
+        ,
+        bootstrapMD_css: "empty:",
+        'font-awesome': "empty:",
+        animate: "empty:"
+    }
+})
