e23d2a367a5bfe17d8adaf5337dd31272acfab3c
Oscar Gardiazabal <trollderiu@gmail.com>
2018-05-19 19:59:11 -0400

    bug bootstrap-material-design popper.js

--- src/simple_main.js
+++ src/simple_main.js
@@ -15,7 +15,6 @@ define(function () {
             //bootstrap: "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.bundle",
             bootstrapMD: "https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design",
             axios: "https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min",
-            'popper.js': "https://unpkg.com/popper.js@1.12.6/dist/umd/popper",
             bootstrap_notify: "https://cdnjs.cloudflare.com/ajax/libs/mouse0270-bootstrap-notify/3.1.7/bootstrap-notify.min",
             sweetalert: "https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min",
             // CSS
@@ -55,6 +54,11 @@ define(function () {
     define("axios-global", ["axios"], function (axios) {
         window.axios = axios;
     });
+    //seems bugged bootstrap-material-design popper.js
+    define("popper.js", ["https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js"], function(popper){
+        window.Popper = popper;
+        return popper;
+    })
 
     define("boot", [
         // CSS
@@ -65,16 +69,16 @@ define(function () {
         "jquery",
         "vue-global",
         "axios-global",
-        "notify",
-        //"bootstrap"
-        "bootstrapMD"
+        "notify"
     ]);
 
     // NOT ESSENTIAL AND/OR DELAYABLE LIBS
     requirejs([
         "text",
         "css",
-        "vue"
+        "vue",
+        //"bootstrap"
+        "bootstrapMD"
     ]);
 
 });
